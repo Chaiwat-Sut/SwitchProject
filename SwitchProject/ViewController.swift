7f2b11ccbee8b27fcd2dd5f090d9bc64b9ff9cab
Chaiwat Suttiprapha <chaiwat.s@arise.tech>
2024-02-06 11:29:29 +0700

    Initial Commit

---
+++ SwitchProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwitchProject
+//
+//  Created by A667459 A667459 on 6/2/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
